f3b305fa0ec37de3bfa3a67b98a066d5c5d98b33
mar1332244 <marcelo1332244@gmail.com>
2025-01-10 00:26:14 -0800

    Began work on layout of program

--- main.go
+++ main.go
@@ -1,9 +1,82 @@
 package main
 
 import (
-    "fmt"
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
+type Command struct {
+    Parameter int
+    Type      CommandType
+}
+
+type Program []Command
+
+func IsWhitespaceChar(char byte) bool {
+    return char == Space || char == Tab || char == Newline
+}
+
+func ExtractWhitespaceFromFile(filepath string) ([]byte, error) {
+
+    const InitialBufferSize int = 256
+
+    file, err := os.Open(filepath)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    reader := bufio.NewReader(file)
+    whitespaceChars := make([]byte, 0, InitialBufferSize)
+
+    for {
+        currentByte, err := reader.ReadByte()
+
+        if err != nil {
+            if errors.Is(err, io.EOF) {
+                break
+            }
+
+            return nil, fmt.Errorf("[Error] Failed to read file `%s` - %w", filepath, err)
+        }
+
+        if IsWhitespaceChar(currentByte) {
+            whitespaceChars = append(whitespaceChars, currentByte)
+        }
+    }
+
+    return whitespaceChars, nil
+}
+
+func ParseWhitespaceChars(whitespaceChars []byte) (Program, error) {
+    return nil, nil
+}
+
+func InterpretProgram(program Program) (exitCode int) {
+    return 0
+}
+
 func main() {
-    fmt.Println(commands)
+
+    if len(os.Args) < 2 {
+        fmt.Fprintln(os.Stderr, "[Error] No Whitespace file provided. Aborting")
+        os.Exit(1)
+    }
+
+    whitespaceChars, err := ExtractWhitespaceFromFile(os.Args[1])
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err.Error())
+        os.Exit(1)
+    }
+
+    program, err := ParseWhitespaceChars(whitespaceChars)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err.Error())
+        os.Exit(1)
+    }
+
+    os.Exit(InterpretProgram(program))
 }
